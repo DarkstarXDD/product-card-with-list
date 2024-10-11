113e990d1e7ccccbdc08530a294c6e9ee927013a
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-10-12 00:47:48 +0530

    refactor: style updates

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -6,20 +6,22 @@ function CartItem({ id, name, itemCount, price, onRemove }) {
   const subTotal = Number(itemCount) * Number(price)
 
   return (
-    <li className={styles.itemWrapper}>
-      <h3 className={styles.itemName}>{name}</h3>
-      <p className={styles.details}>
-        <span className={styles.itemCount}>{itemCount}x</span>
-        <span className={styles.itemPrice}>@ ${price.toFixed(2)}</span>
-        <span className={styles.subTotal}>${subTotal.toFixed(2)}</span>
-      </p>
-      <button
-        aria-label="clear item"
-        onClick={() => onRemove(id)}
-        className={styles.button}
-      >
-        <IoCloseSharp className={styles.buttonIcon} />
-      </button>
+    <li className={styles.itemOuterWrapper}>
+      <div className={styles.itemInnerWrapper}>
+        <h3 className={styles.itemName}>{name}</h3>
+        <p className={styles.details}>
+          <span className={styles.itemCount}>{itemCount}x</span>
+          <span className={styles.itemPrice}>@ ${price.toFixed(2)}</span>
+          <span className={styles.subTotal}>${subTotal.toFixed(2)}</span>
+        </p>
+        <button
+          aria-label="clear item"
+          onClick={() => onRemove(id)}
+          className={styles.button}
+        >
+          <IoCloseSharp className={styles.buttonIcon} />
+        </button>
+      </div>
       <div className={styles.divider}></div>
     </li>
   )
