72c49a915d42d215d4541c44d076ed16b7726866
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-10-12 23:51:36 +0530

    refactor: simplify the html structure for modalItem

--- src/components/ModalItem/ModalItem.jsx
+++ src/components/ModalItem/ModalItem.jsx
@@ -2,17 +2,15 @@ import styles from "./ModalItem.module.css"
 
 export default function ModalItem() {
   return (
-    <li className={styles.itemOuterWrapper}>
-      <div className={styles.itemInnerWrapper}>
-        <div className={styles.imageNameWrapper}>
+    <li className={styles.item}>
+      <div className={styles.itemOuterWrapper}>
+        <div className={styles.itemInnerWrapper}>
           <img src="" alt="" className={styles.thumbnail} />
-          <div className={styles.detailsOuterWrapper}>
-            <h3 className={styles.itemName}>Classic Tiramisu</h3>
-            <p className={styles.detailsInnerWrapper}>
-              <span className={styles.itemCount}>1x</span>
-              <span className={styles.price}>@ $5.50</span>
-            </p>
-          </div>
+          <h3 className={styles.itemName}>Classic Tiramisu</h3>
+          <p className={styles.countPriceWrapper}>
+            <span className={styles.itemCount}>1x</span>
+            <span className={styles.price}>@ $5.50</span>
+          </p>
         </div>
         <p className={styles.subTotal}>$5.50</p>
       </div>
